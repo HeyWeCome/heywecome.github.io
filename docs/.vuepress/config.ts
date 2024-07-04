172b09f2721cf4ac1fc928c6416726246427d617
heywecome <heywecome@163.com>
2024-07-04 10:03:07 +0800

    添加数学公式支持

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,6 +8,7 @@ import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
 
+
 const DOMAIN_NAME = 'heywecome.github.io/' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
 
@@ -267,6 +268,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       {
         transformer: (timestamp, lang) => {
           return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      },
+    ],
+    [
+      'vuepress-plugin-mathjax', // markdown数学公式
+      {
+        target: 'svg',
+        macros: {
+          '*': '\\times',
         },
       },
     ],
